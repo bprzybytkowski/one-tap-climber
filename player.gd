2dd61a3d4c935e5ee133bde8743eef6e35b567db
bprzybytkowski <bprzybytkowski@gmail.com>
2023-04-12 23:56:45 +0200

    Add player, floor and walls. Move the player, bounce of the walls and add jump

---
+++ player.gd
@@ -0,0 +1,30 @@
+extends CharacterBody2D
+
+
+const SPEED = 300.0
+const JUMP_VELOCITY = -400.0
+
+var direction = 1
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Handle Jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+	
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	move_and_slide()
+
+func _on_area_2d_body_entered(body):
+	if body.name == "Walls":
+		direction *= -1
